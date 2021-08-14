c42320517faf2652f8ac13b9eea01b27b1e6d29b
Matheus OLiveira <matheusoliveira@pop-os.localdomain>
2021-08-14 15:41:31 -0300

    inicalizando-repo

---
+++ src/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,39 @@
+import { client } from '../../prisma/client'
+import { hash } from 'bcryptjs'
+
+interface IUserRequest {
+  name: string;
+  password: string;
+  username: string;
+}
+
+class CreateUserUseCase {
+
+  async execute({ name, username, password }: IUserRequest) {
+    //Verificando se o usuário existe
+    const userExistis = await client.user.findFirst({
+      where: {
+        username,
+      }
+    })
+
+    if (userExistis) {
+      throw new Error("Usuario existente")
+    }
+    //Cadastro do usuario
+    // Criptografia da senha
+    const passwordHash = await hash(password, 8)
+
+    const user = await client.user.create({
+      data: {
+        name,
+        username,
+        password: passwordHash
+      }
+    })
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase }
